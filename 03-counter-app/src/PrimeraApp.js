bd4f5ecf4f051185e77c7eeecdae6446fbd0c1a6
Daniel <dachaparro@uc.cl>
2022-04-03 23:37:20 -0400

    react firsts

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,7 +1,7 @@
 // Functional components
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-
+// Fragmentes se pued evadir poniendo <> </>  remplazand divs
 
 
 const PrimeraApp= () => {

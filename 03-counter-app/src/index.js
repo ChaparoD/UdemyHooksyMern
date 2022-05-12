3a4cb7bf9bfb9752532063f487645f6c156cfa20
Daniel <dachaparro@uc.cl>
2022-05-12 19:16:39 -0400

    createRoot

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom'; 
+//Sacamos ReactDom.render(<fatherComponent>, divRoot)
+import { createRoot } from 'react-dom/client';
 //import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 import './index.css';
 
 
 const divRoot = document.querySelector('#app')
-console.log(divRoot)
+const root = createRoot(divRoot);
 
-ReactDOM.render(<CounterApp  />, divRoot);
+root.render(<CounterApp />);
